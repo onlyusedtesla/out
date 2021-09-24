fbd77c4aaa97d7813fe6b79809a815f53390a872
Glitch (hello-sqlite) <none>
2021-09-24 07:49:16 +0000

    🎎🤗 Checkpoint ./public/home.js:328969/2927 ./public/style.css:328969/240 ./views/index.ejs:328969/96

--- public/home.js
+++ public/home.js
@@ -1,62 +1,15 @@
-// client-side js
-// run by the browser each time your view template referencing it is loaded
-
-console.log("hello world :o");
-
-const dreams = [];
-
-// define variables that reference elements on our page
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements["dream"];
-const dreamsList = document.getElementById("dreams");
-const clearButton = document.querySelector('#clear-dreams');
-
-// request the dreams from our app's sqlite database
-fetch("/getDreams", {})
-  .then(res => res.json())
-  .then(response => {
-    response.forEach(row => {
-      appendNewDream(row.dream);
-    });
+(function () {
+  
+  const searchInput = document.querySelector(".js-searchbox-input");
+  const searchBox = document.querySeletor(".js-searchbox");
+  
+  searchInput.addEventListener('focus', function (event) {
+    searchBox.classList.add("searchbox--focused");
   });
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = dream => {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
-};
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = event => {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  const data = { dream: dreamInput.value };
-
-  fetch("/addDream", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
-  })
-    .then(res => res.json())
-    .then(response => {
-      console.log(JSON.stringify(response));
-    });
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form
-  dreamInput.value = "";
-  dreamInput.focus();
-};
-
-clearButton.addEventListener('click', event => {
-  fetch("/clearDreams", {})
-    .then(res => res.json())
-    .then(response => {
-      console.log("cleared dreams");
-    });
-  dreamsList.innerHTML = "";
-});
+  
+  searchInput.addEventListener('blur', function (event) {
+    searchBox.classList.remove("searchbox--focused");
+  });
+  
+  // when you focus on the search inbox, then add a class to the outer element, and remove it when you lose focus.
+})();
