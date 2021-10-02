5dfee1bfe51d2294048b649b12db75b72afb4599
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-02 09:10:38 +0000

    🐦🙋 Checkpoint ./server.js:328969/1021 ./saver.js:328969/863 ./db.js:328969/26

--- server.js
+++ server.js
@@ -5,14 +5,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const fs = require("fs");
 const { auth } = require('express-openid-connect');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -31,36 +27,6 @@ const config = {
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
-
-// init sqlite db
-const dbFile = "./.data/sqlite.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
-
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(() => {
-  if (!exists) {
-    db.run(
-      "CREATE TABLE Dreams (id INTEGER PRIMARY KEY AUTOINCREMENT, dream TEXT)"
-    );
-    console.log("New table Dreams created!");
-
-    // insert default dreams
-    db.serialize(() => {
-      db.run(
-        'INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
-      );
-    });
-  } else {
-    console.log('Database "Dreams" ready to go!');
-    db.each("SELECT * from Dreams", (err, row) => {
-      if (row) {
-        console.log(`record: ${row.dream}`);
-      }
-    });
-  }
-});
 
 app.get("/", (request, response) => {
   if (typeof request.query.search !== "undefined" && request.query.search.length >= 1) {
