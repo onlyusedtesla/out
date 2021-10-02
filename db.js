0a4a3d76bafbca3ed8bd9f517cfbfb27a835757f
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-02 09:55:51 +0000

    🌌🐁 Checkpoint ./db.js:328969/1621

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const Database = require('better-sqlite3');
 const db = new Database('teslatracker.db', { verbose: console.log });
 
-const stmt = db.prepare('INSERT INTO cats (name, age) VALUES (?, ?)');
-const info = stmt.run('Joey', 2);
+const stmt = db.prepare("CREATE TABLE IF NOT EXISTS items('title' VARCHAR, 'description' VARCHAR, 'url' VARCHAR, 'item_id' INTEGER PRIMARY KEY AUTOINCREMENT, 'tags' VARCHAR, 'link_type' VARCHAR, 'timestamp' DEFAULT CURRENT_TIMESTAMP, 'item_date' VARCHAR)");
+const info = stmt.run();
 
-console.log(info.changes); // => 1
+const stmt2 = db.prepare('INSERT INTO items (title, description, url, domain, date, tags, link_type) VALUES (@title, @description, @url, @domain, @date, @tags, @link_type)');
 
-db.prepare("CREATE TABLE IF NOT EXISTS items('title' VARCHAR, 'description' VARCHAR, 'url' VARCHAR, 'item_id' INTEGER PRIMARY KEY AUTOINCREMENT, 'tags' VARCHAR, 'link_type' VARCHAR, 'timestamp' DEFAULT CURRENT_TIMESTAMP, 'item_date' VARCHAR)");
+const items = [{
+  title: 
+}];
+  
+  
+console.log(info); // => 1
