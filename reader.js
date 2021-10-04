93de53d771921a9281b11d063e1acf36b3cddedb
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-04 15:22:00 +0000

    🍱💈 Checkpoint ./reader.js:328969/405 ./package.json:328969/1648

--- reader.js
+++ reader.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
 const { parse } = require('rss-to-json');
 const save = require('./db.js');
+import { stripHtml } from "string-strip-html";
+
 const rawData = fs.readFileSync(__dirname + '/data.json');
 const data = JSON.parse(rawData);
-
 parse("https://feedbin.com/starred/c5abfc079595d929aa9a1ef735cccd7b.xml").then(function (rss) {
   // save();
   console.log("What's the rss from feedbin?", rss);
+  
+  save(rss.items);
+  
+  let items = rss.items.map(function (item) {
+    item.domain = item.link;
+    item.description = item.
+  });
+  
 }).catch(function (error) {
   console.log("There's been an error");
   console.log("error", error);
