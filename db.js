34ac1c0e3986e9600e23ef1a825bb35f1204f9c4
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-04 16:09:12 +0000

    🐦👕 Checkpoint ./reader.js:328969/519 ./package.json:328969/1675 ./db.js:328969/125

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const Database = require('better-sqlite3');
 const db = new Database('teslatracker.db', { verbose: console.log });
 
+const stmt = db.prepare("PRAGMA table_info(items)");
+stmt.run();
+
 function save(items) {
-  const insert = db.prepare('INSERT INTO items (title, description, url, domain, date, tags, link_type) VALUES (@title, @description, @url, @domain, @date, @tags, @link_type)');
+  const insert = db.prepare('INSERT INTO items (title, description, url, date, tags, link_type) VALUES (@title, @description, @url, @date, @tags, @link_type)');
   
   db.transaction(function (items) {
     for (let i = 0; i < items.length; i += 1) {
--- reader.js
+++ reader.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { parse } = require('rss-to-json');
 const save = require('./db.js');
-import { stripHtml } from "string-strip-html";
+const striptags = require('striptags');
 
 const rawData = fs.readFileSync(__dirname + '/data.json');
 const data = JSON.parse(rawData);
@@ -9,16 +9,15 @@ parse("https://feedbin.com/starred/c5abfc079595d929aa9a1ef735cccd7b.xml").then(f
   // save();
   console.log("What's the rss from feedbin?", rss);
   
-  save(rss.items);
-  
   let items = rss.items.map(function (item) {
-    item.domain = item.link;
-    item.description = stripHtml(item.description);
+    item.url = item.link;
+    item.description = striptags(item.description);
     item.link_type = "article";
     item.date = item.published;
     item.tags = "";
-    
   });
+  
+  save(items);
   
 }).catch(function (error) {
   console.log("There's been an error");
