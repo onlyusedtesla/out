fa90c606d7661847fdae158ab910b4df824ce5ca
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-04 17:28:00 +0000

    🌏🚙 Checkpoint ./data.json:328969/10

--- db-test.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('teslatracker.db', { verbose: console.log });
-
-const items = db.prepare('SELECT * FROM items').all();
-
-console.log("items", items);
