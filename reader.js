108bce0ccef798fe6fcfd6b40cc32dc2651ddc4f
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-04 14:48:13 +0000

    🎀🌯 Checkpoint ./reader.js:328969/1376 ./package.json:328969/1686

--- reader.js
+++ reader.js
@@ -1,7 +1,17 @@
-const https = require('https');
 const fs = require('fs');
 const parser = require("fast-xml-parser");
 
+const { parse } = require('rss-to-json');
+// async await
+(async () => {
+
+    var rss = await parse('https://blog.ethereum.org/feed.xml');
+
+    console.log(JSON.stringify(rss, null, 3));
+
+})();
+
+parse
 const options = {
   hostname: 'feedbin.com',
   port: 443,
@@ -12,44 +22,8 @@ const options = {
 const rawData = fs.readFileSync(__dirname + '/data.json');
 const data = JSON.parse(rawData);
 
+//  if (res.headers.etag !== data.etag) {
+   
+//  }
+
 console.log("What's the data?", data);
-
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`);
-  
-  console.log('res.headers', res.headers);
-  
-    res.on('data', d => {
-      // const feed = parser.parse(d.toString());
-      console.log("toString", d.toString());
-      // console.log("What's the feed?", feed);
-    });
-  
-//   // Only read the data if it's been modified
-//   if (res.headers.etag !== data.etag) {
-    
-//     data.etag = res.headers.etag;
-    
-//     res.on('data', d => {
-//       console.log("What's the data?", d);
-//       process.stdout.write(d);
-      
-//       const feed = parser.parse(d);
-//       console.log("What's the feed?", feed);
-      
-//     });
-    
-//     fs.writeFileSync(__dirname + '/data.json', JSON.stringify(data));
-    
-//   } else {
-//     console.log("The etag is the same, so the data is the same don't read this file.");
-//     console.log("etag", data.etag);
-//   }
-  
-})
-
-req.on('error', error => {
-  console.error(error);
-})
-
-req.end();
