f813908a3e0b52e67b96291e67b18b80726a0a3e
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-04 14:59:29 +0000

    🛫👲 Checkpoint ./reader.js:328969/834

--- reader.js
+++ reader.js
@@ -1,29 +1,10 @@
 const fs = require('fs');
-const parser = require("fast-xml-parser");
-
 const { parse } = require('rss-to-json');
-// async await
-(async () => {
-
-    var rss = await parse('https://blog.ethereum.org/feed.xml');
-
-    console.log(JSON.stringify(rss, null, 3));
-
-})();
-
-parse
-const options = {
-  hostname: 'feedbin.com',
-  port: 443,
-  path: '/starred/c5abfc079595d929aa9a1ef735cccd7b.xml',
-  method: 'GET'
-}
 
 const rawData = fs.readFileSync(__dirname + '/data.json');
 const data = JSON.parse(rawData);
 
-//  if (res.headers.etag !== data.etag) {
-   
-//  }
+parse("https://feedbin.com/starred/c5abfc079595d929aa9a1ef735cccd7b.xml").then(function (rss) {
+  console.log("What's the rss from feedbin?", rss);
+});
 
-console.log("What's the data?", data);
