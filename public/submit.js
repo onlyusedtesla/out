7846b71b12008f18c5e1fb99a377dacf48f7f765
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-04 20:33:25 +0000

    🚣🐠 Checkpoint ./public/submit.js:328969/601 ./views/submit.ejs:328969/68

--- public/submit.js
+++ public/submit.js
@@ -1,8 +1,20 @@
 (function () {
-  var form = document.querySelector(".js-form");
+  var form = document.querySelector(".js-form"),
+      url = document.querySelector(".js-url"),
+      title = document.querySelector(".js-title"),
+      description = document.querySelector(".js-description");
   
   form.addEventListener('submit', function (event) {
     event.preventDefault();
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/submit", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+      url: url,
+      title: title,
+      description: description
+    }));
     
   });
   
