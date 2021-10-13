8386520d14ff9e1e64fad90f693ab1a34817c434
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-13 18:43:40 +0000

    🏀🕹 Checkpoint ./db2.js:328969/21

--- db2.js
+++ db2.js
@@ -2,7 +2,8 @@
 
 // So the first thing that I need to do is to create two tables with the various columns, and then go through each of the 
 // 
-const db = require('better-sqlite3')('foobar.db', options);
+const db = require('better-sqlite3')('teslatracker.db', options);
 
 const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
+
 console.log(row.firstName, row.lastName, row.email);
