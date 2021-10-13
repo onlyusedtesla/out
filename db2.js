456de0bbe5c100bf3a5aba207ef0190373c46ccd
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-13 18:54:55 +0000

    🛤🛩 Checkpoint ./db2.js:328969/661 ./data.json:328969/148609

--- db2.js
+++ db2.js
@@ -1,9 +1,22 @@
 // The goal here is to transfer all the stuff into a SQLite way of saving, rather than using JSON files.
 
 // So the first thing that I need to do is to create two tables with the various columns, and then go through each of the 
-// 
-const db = require('better-sqlite3')('teslatracker.db', options);
 
-const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
+const db = require('better-sqlite3')('teslatracker.db');
 
-console.log(row.firstName, row.lastName, row.email);
+function createItemsTable() {
+  const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
+  console.log(row.firstName, row.lastName, row.email);
+}
+
+function createSubmissionsTable() {
+  
+}
+
+function addExistingItems() {
+  
+}
+
+function addExistingSubmissions() {
+  
+}
