4217f116c162aa7bb899f823700a00b1e8a2ff17
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-15 17:40:38 +0000

    🐊⛺️ Checkpoint ./public/style.css:328969/949 ./public/search.js:328969/2073

---
+++ public/search.js
@@ -0,0 +1,27 @@
+(function () {
+  
+  const searchInput = document.querySelector(".js-searchbox-input");
+  const searchBox = document.querySelector(".js-searchbox");
+  const mobileSearchBox = document.querySelector(".js-searchbutton-mobile");
+  
+  // when you focus on the search inbox, then add a class to the outer element, and remove it when you lose focus.
+  searchInput.addEventListener("focus", function(event) {
+    searchBox.classList.add("searchbox--focused");
+  });
+
+  searchInput.addEventListener("blur", function(event) {
+    searchBox.classList.remove("searchbox--focused");
+  });
+  
+  mobileSearchBox.addEventListener("click", function(event) {
+    event.preventDefault();
+    var searchTerms = prompt("Enter search");
+
+    if (searchTerms && searchTerms.length >= 1) {
+      var url = new URL(window.location.href);
+      url.searchParams.set("search", encodeURIComponent(searchTerms));
+      window.location.href = url.href;
+    }
+  });
+  
+}());
