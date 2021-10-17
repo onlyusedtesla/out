cb895dd4fa22799b007179fc0784f7ffa817eace
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-17 05:54:37 +0000

    🙆🍩 Checkpoint ./favorites.json:328969/164 ./.gitignore:328969/10 ./data.json:328969/168833 ./data-transform.js:328969/1006

---
+++ data-transform.js
@@ -0,0 +1,6 @@
+const data = require("./data.json");
+const favorites = require("./favorites.json");
+const upvotes = require("./upvotes.json");
+
+console.log("Gonna transform some JSON files in here.");
+console.log("What's data?", data);
