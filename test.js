2340f1b3d7dd38379382831b3cb1a7763890d8ec
Glitch (mountain-rhetorical-trumpet) <none>
2021-10-28 08:25:57 +0000

    🌮🖐 Checkpoint ./reader.js:328969/30 ./test.js:328969/44

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const reader = require("./reader.js");
 
 // reader();
 
-reader.getFavicon("yelp.com").then(function (image) {
+reader.getFavicon("github.com").then(function (image) {
   console.log("What's the image?", image);
 });
