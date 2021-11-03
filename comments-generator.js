6ece61750057b6dcbdc4e3bce7593901fc2781fe
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-03 08:55:01 +0000

    📈🎺 Checkpoint ./comments-generator.js:328969/1074

--- comments-generator.js
+++ comments-generator.js
@@ -2,18 +2,32 @@ const fs = require("fs");
 
 let itemId = "0b914140";
 let author = "Jack";
+let contents = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis.";
+let comment_date = "1635450631000";
 
-      "contents": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis.",
-      "comment_date": "1635450631000",
-let comments = {};
+let comments = [];
 
+function uuid() {
+  return 'xxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
+for (let i = 0; i < 50; i += 1) {
+  comments.push({
+    item_id: itemId,
+    author: author,
+    contents: contents,
+    comment_date: comment_date,
+    comment_id: uuid()
+  });
+}
+
+let commentIds = 
+    
     {
       "comment_id": "81gbh",
       "item_id": "499jfjd",
       "replies": [5940, 45040, 40540504, 45405045]
-    },
-
-comments[""]
-comments["comments"] = {};
-
+    }
