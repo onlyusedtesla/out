75997ba12fb5c53c4f7c69e88160ed6c36e7342b
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-04 11:49:04 +0000

    🎊🕷 Checkpoint ./views/partials/comments.ejs:328969/3064 ./public/comments.js:328969/298 ./views/comments.ejs:328969/117

--- public/comments.js
+++ public/comments.js
@@ -1,7 +1,7 @@
 (function () {
   
   const timeEls = document.querySelectorAll(".js-comment-date");
-  const comments = document.querySelectorAll(".js-comment");
+  const comments = document.querySelectorAll(".js-commentContainer");
   
   function hideAllCommentBoxes() {
     const commentBoxes = Array.from(document.querySelectorAll(".js-comment-form:not(.js-comment-form-primary)"));
@@ -16,10 +16,12 @@
     el.innerHTML = dateFormat(+el.getAttribute("datetime"), "mmm d, h:MM TT");
   });
   
-  Array.from(comments).forEach(function (comment) {
-    const replyButton = comment.querySelector(".js-comment-reply-button");
-    const commentBox = comment.querySelector(".js-comment-form");
-    replyButton.addEventListener('click', function () {
+  Array.from(comments).forEach(function (el) {
+    console.log("el", el);
+    const replyButton = el.querySelector(".js-comment-reply-button");
+    const commentBox = el.querySelector(".js-comment-form");
+    replyButton.addEventListener('click', function (event) {
+      event.preventDefault();
       hideAllCommentBoxes();
       commentBox.classList.remove("hidden");
     });
