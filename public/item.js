822d13a58c2c68761a1f536cc347a7cce4bd823f
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-04 21:51:09 +0000

    🍀😘 Checkpoint ./views/index.ejs:328969/190 ./public/global.js:328969/13816 ./views/submit.ejs:328969/24 ./public/search.js:328969/943 ./public/item.js:328969/1008 ./views/comments.ejs:328969/126

---
+++ public/item.js
@@ -0,0 +1,277 @@
+(function() {
+  const moreButton = document.querySelector(".js-more-button"),
+        hoverTooltips = document.querySelectorAll(".js-tooltip-hover"),
+        mobileToggle = document.querySelector(".js-mobilemenutoggle"),
+        mobileNavigation = document.querySelector(".js-mobilenavigation");
+  
+  function removeFavorite(itemId) {
+    return new Promise(function(resolve, reject) {
+      fetch("/removeFavorite?item_id=" + itemId, {
+        method: "POST"
+      }).then(res => {
+        console.log("What's the response?", res);
+
+        if (res.status === 200) {
+          res.text().then(function(html) {
+            resolve(html);
+          });
+        } else {
+          reject(res.body);
+        }
+      });
+    });
+  }
+
+  function favoriteItem(itemId) {
+    console.log("Calling the favoriteItem function for " + itemId);
+
+    return new Promise(function(resolve, reject) {
+      fetch("/addFavorite?item_id=" + itemId, {
+        method: "POST"
+      }).then(res => {
+        console.log("What's the response?", res);
+
+        if (res.status === 200) {
+          res.text().then(function(html) {
+            resolve(html);
+          });
+        } else {
+          reject(res.body);
+        }
+      });
+    });
+  }
+
+  function upvoteItem(itemId) {
+    console.log("Calling the upvoteItem function for " + itemId);
+
+    return new Promise(function(resolve, reject) {
+      fetch("/upvote?item_id=" + itemId, {
+        method: "POST"
+      }).then(res => {
+        console.log("What's the response?", res);
+
+        if (res.status === 200) {
+          res.text().then(function(html) {
+            resolve(html);
+          });
+        } else {
+          reject(res.body);
+        }
+      });
+    });
+  }
+
+  function removeUpvote(itemId) {
+    return new Promise(function(resolve, reject) {
+      fetch("/removeUpvote?item_id=" + itemId, {
+        method: "POST"
+      }).then(res => {
+        console.log("What's the response?", res);
+
+        if (res.status === 200) {
+          res.text().then(function(html) {
+            resolve(html);
+          });
+        } else {
+          reject(res.body);
+        }
+      });
+    });
+  }
+  
+  function tooltipHandler(event) {
+    
+    var tooltip = event.target;
+    
+    event.preventDefault();
+    tooltip.classList.add("show");
+    setTimeout(function() {
+      tooltip.classList.remove("show");
+    }, 2100);
+  }
+  
+  function addTooltipListeners() {
+    var tooltips = document.querySelectorAll(".js-tooltip");
+    Array.from(tooltips).forEach(function(tooltip) {
+      tooltip.removeEventListener("click", tooltipHandler);
+      tooltip.addEventListener("click", tooltipHandler);
+    });
+  }
+  
+  Array.from(hoverTooltips).forEach(function(tooltip) {
+    tooltip.addEventListener("mouseover", function(event) {
+      event.preventDefault();
+      tooltip.classList.add("show");
+    });
+
+    tooltip.addEventListener("mouseout", function(event) {
+      event.preventDefault();
+      tooltip.classList.remove("show");
+    });
+  });
+  
+  function shareHandler(event) {
+    var share = event.target;
+    
+    event.preventDefault();
+    navigator.clipboard
+      .writeText(share.getAttribute("data-link"))
+      .then(function() {}, function() {});
+  }
+  
+  function addShareListeners() {
+    var shares = document.querySelectorAll(".js-share");
+    
+    Array.from(shares).forEach(function(share) {
+      share.removeEventListener('click', shareHandler);
+      share.addEventListener("click", shareHandler);
+    });
+  }
+  
+  function toggleUpvotes(upvoteEls) {
+    Array.from(upvoteEls).forEach(function(button) {
+      const svg = button.querySelector("svg > path"),
+        count = button.parentNode.querySelector(".js-upvoteCount"),
+        isNotUpvoted = svg.getAttribute("data-upvoted") === "false";
+
+      console.log("What's the countEl?", count);
+
+      if (isNotUpvoted) {
+        svg.setAttribute("fill", svg.getAttribute("data-fill-upvoted"));
+        button.classList.add("article-share--upvoted");
+        svg.setAttribute("data-upvoted", "true");
+        button.setAttribute("data-action", "remove");
+        count.innerHTML = +count.innerHTML + 1;
+      } else {
+        svg.setAttribute("fill", svg.getAttribute("data-fill-notupvoted"));
+        button.classList.remove("article-share--upvoted");
+        svg.setAttribute("data-upvoted", "false");
+        button.setAttribute("data-action", "add");
+        count.innerHTML = +count.innerHTML - 1;
+      }
+    });
+  }
+  
+  function upvoteHandler(event) {
+    event.preventDefault();
+    
+    var upvote = event.target;
+    
+    var article = upvote.closest(".js-article"),
+      upvotesForArticle = article.querySelectorAll(".js-upvote"),
+      itemId = article.getAttribute("data-item-id"),
+      action = upvote.getAttribute("data-action");
+
+    toggleUpvotes(upvotesForArticle);
+
+    console.log("action", action);
+
+    if (action === "add") {
+      // Turn it off if error from the server.
+      upvoteItem(itemId).catch(function() {
+        toggleUpvotes(upvotesForArticle);
+      });
+    } else {
+      removeUpvote(itemId).catch(function() {
+        toggleUpvotes(upvotesForArticle);
+      });
+    }
+  }
+  
+  function addUpvoteListeners() {
+    
+    var upvotes = document.querySelectorAll(".js-upvote");
+    
+    Array.from(upvotes).forEach(function(upvote) {
+      upvote.removeEventListener("click", upvoteHandler);
+      upvote.addEventListener("click", upvoteHandler);
+    });
+  }
+  
+  function convertArticleDates(articleDates) {
+    var articleDates = document.querySelectorAll(".js-article-date")
+    Array.from(articleDates).forEach(function(articleDate) {
+      articleDate.innerHTML = dateFormat(
+        +articleDate.getAttribute("data-time"),
+        "mmm d, h:MM TT"
+      );
+      articleDate.setAttribute(
+        "title",
+        dateFormat(+articleDate.getAttribute("data-time"), "mmm d, h:MM TT Z")
+      );
+    });
+  }
+
+  function toggleFavorite(favoriteEls) {
+    Array.from(favoriteEls).forEach(function(favoriteButton) {
+      const isNotFavorited = favoriteButton.getAttribute("fill") === "none";
+
+      if (isNotFavorited) {
+        favoriteButton.setAttribute(
+          "fill",
+          favoriteButton.getAttribute("data-original-fill")
+        );
+        favoriteButton.setAttribute("stroke", "none");
+      } else {
+        favoriteButton.setAttribute(
+          "stroke",
+          favoriteButton.getAttribute("data-original-stroke")
+        );
+        favoriteButton.setAttribute("fill", "none");
+      }
+    });
+  }
+
+  // Takes care of favoriting.
+  
+  function favoriteHandler(event) {
+    event.preventDefault();
+    
+    var favoriteButton = event.target;
+    
+    var article = favoriteButton.closest(".js-article"),
+      favoritesForArticle = article.querySelectorAll(".js-favorite > path"),
+      itemId = article.getAttribute("data-item-id"),
+      action = favoriteButton.getAttribute("data-action");
+
+    toggleFavorite(favoritesForArticle);
+
+    if (action === "add") {
+      favoriteItem(itemId)
+        .then(function(response) {})
+        .catch(function(error) {
+          toggleFavorite(favoritesForArticle); // put the state of the button back.
+        });
+    } else if (action === "remove") {
+      console.log("going to unfavorite the item.");
+      removeFavorite(itemId)
+        .then(function(response) {})
+        .catch(function(error) {
+          toggleFavorite(favoritesForArticle);
+        });
+    }
+  }
+  
+  function addFavoriteListeners() {
+    var favoriteButtons = document.querySelectorAll(".js-favorite");
+    
+    Array.from(favoriteButtons).forEach(function(favoriteButton) {
+      favoriteButton.removeEventListener("click", favoriteHandler);
+      favoriteButton.addEventListener("click", favoriteHandler);
+    });
+  }
+  
+  mobileToggle.addEventListener("click", function(event) {
+    mobileToggle.classList.toggle("mobile-menuToggle--activated");
+    mobileNavigation.classList.toggle("mobile-navigation--opened");
+  });
+
+  convertArticleDates();
+  addFavoriteListeners();
+  addUpvoteListeners();
+  addShareListeners();
+  addTooltipListeners();
+  
+})();
+
