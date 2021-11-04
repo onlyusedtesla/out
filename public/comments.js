de7c86570c4eb8977da721f5b5f44a47427247a9
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-04 14:49:14 +0000

    💐💨 Checkpoint ./views/partials/comments.ejs:328969/153 ./views/comments.ejs:328969/97 ./public/comments.js:328969/2000

--- public/comments.js
+++ public/comments.js
@@ -2,12 +2,42 @@
   
   const timeEls = document.querySelectorAll(".js-comment-date");
   const comments = document.querySelectorAll(".js-commentContainer");
+  const allCommentBoxes = document.querySelectorAll(".js-comment-form");
   
   function hideAllCommentBoxes() {
-    const commentBoxes = Array.from(document.querySelectorAll(".js-comment-form:not(.js-comment-form-primary)"));
+    const commentBoxes = document.querySelectorAll(".js-comment-form:not(.js-comment-form-primary)");
     
     commentBoxes.forEach(function (el) {
       el.classList.add("hidden");
+    });
+  }
+  
+  function serialize(obj) {
+    var str = [];
+    for (var p in obj)
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      }
+    return str.join("&");
+  }
+  
+  function addComment(comment) {
+    return new Promise(function(resolve, reject) {
+      fetch("/comment?" + serialize(comment), {
+        method: "POST"
+      }).then(res => {
+        console.log("What's the response?", res);
+        
+        return false;
+        
+        if (res.status === 200) {
+          res.text().then(function(html) {
+            resolve(html);
+          });
+        } else {
+          reject(res.body);
+        }
+      });
     });
   }
   
@@ -17,13 +47,21 @@
   });
   
   Array.from(comments).forEach(function (el) {
-    console.log("el", el);
     const replyButton = el.querySelector(".js-comment-reply-button");
     const commentBox = el.querySelector(".js-comment-form");
     replyButton.addEventListener('click', function (event) {
       event.preventDefault();
       hideAllCommentBoxes();
       commentBox.classList.remove("hidden");
+    });
+  });
+  
+  Array.from(allCommentBoxes).forEach(function (el) {
+    
+    el.addEventListener("submit", function (event) {
+      event.preventDefault();
+      
+      
     });
   });
   
