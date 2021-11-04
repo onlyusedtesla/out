cc1ce5d4e799545698da7e73a7c8ed500312469d
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-04 11:14:39 +0000

    🎳🐋 Checkpoint ./views/partials/comments.ejs:328969/2021 ./views/comments.ejs:328969/84 ./public/comments.js:328969/946

--- public/comments.js
+++ public/comments.js
@@ -2,11 +2,18 @@
   
   const timeEls = document.querySelectorAll(".js-comment-date");
   
+  const comments = document.querySelectorAll(".js-comment");
+  
   console.log("Going to run the dateFormat function on the time elements");
   
   Array.from(timeEls).forEach(function (el) {
     console.log("What's el?", el);
     el.innerHTML = dateFormat(+el.getAttribute("datetime"), "mmm d, h:MM TT");
+  });
+  
+  Array.from(comments).forEach(function (comment) {
+    const replyButton = comment.querySelector(".js-comment-reply-button");
+    replyButton.addEventListener('click', function ());
   });
   
 }());
