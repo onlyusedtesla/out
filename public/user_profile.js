05463cc5ef2f6a018c8e4cdd2f4d90e7db6be499
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-13 11:52:30 +0000

    🛋🗼 Checkpoint ./public/user_profile.js:328969/4647 ./views/user_profile.ejs:328969/248

--- public/user_profile.js
+++ public/user_profile.js
@@ -1,3 +1,36 @@
-(function () {
+(function() {
+  var form = document.querySelector(".js-form"),
+    url = document.querySelector(".js-url"),
+    title = document.querySelector(".js-title"),
+    author = document.querySelector(".js-author"),
+    question = document.querySelector(".js-question"),
+    description = document.querySelector(".js-description"),
+    successMessage = document.querySelector(".js-success-message");
   
-}());
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var responseBody = {
+      title: "Ask TT: " + question.value.trim(),
+      description: description.value,
+      date_published: dateFormat(Date.now()),
+      author: author.value,
+      content_html: contentHtml,
+      isQuestion: true
+    };
+    
+    fetch("/submit", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(responseBody)
+    }).then(res => {
+      if (res.status === 200) {
+        form.classList.add("hidden");
+        successMessage.classList.remove("hidden");
+      }
+    });
+  });
+})();
