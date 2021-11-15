05a6a87cb9e1bf2f8f0472fbc655c0fcd5cd222c
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-15 03:28:29 +0000

    🤑🚴 Checkpoint ./views/user_profile.ejs:328969/265 ./generate-invite-codes.js:328969/346

---
+++ generate-invite-codes.js
@@ -0,0 +1,14 @@
+
+function generateCodes() {
+  // Just call a DB method, and then return the individual codes that were generated.
+  
+}
+
+function getUnusedCodes() {
+  
+}
+
+module.exports = {
+  generateCodes: generateCodes,
+  getUnusedCodes: getUnusedCodes
+};
