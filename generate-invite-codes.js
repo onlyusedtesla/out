c6c33643dacbb5067e50fa68c469f19efe81b379
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-22 16:48:29 +0000

    🎀👢 Checkpoint ./db.js:328969/104 ./reader.js:328969/32 ./generate-invite-codes.js:328969/127

--- generate-invite-codes.js
+++ generate-invite-codes.js
@@ -1,5 +1,6 @@
 const db = require("./db.js");
 
+if (db.getUserI)
 db.generateInviteCodes(5, "adamqureshi");
 
 // console.log("db.getInviteCodes()", db.getInviteCodes("derick.realwebdev+teslatracker"));
