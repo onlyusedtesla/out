90a8f352e47b54090666f669e1835b636748ee42
Glitch (mountain-rhetorical-trumpet) <none>
2021-11-26 04:47:17 +0000

    👢💡 Checkpoint ./convert-px-to-em.js:328969/708 ./set-style-settings.js:328969/357

--- set-style-settings.js
+++ set-style-settings.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 const original = fs.readFileSync(__dirname + "/public/style.css").toString();
 const regex = /--bg-color: (#\w{6})/
+const settings = require('./settings.json');
 
 let newFileText = original.replace(regex, function (match, p1, p2, p3, offset, string) {
-  console.log("What's match?", match);
-  console.log("p1", p1);
-  console.log("p2", p2);
-  console.log("p3", p3);
-  console.log("offset", offset);
-  console.log("string", string);
+  return "--bg-color: " + settings.backgroundColor;
 });
+
+console.log("newFileText", newFileText);
 
 // try {
 //   fs.writeFileSync(__dirname + "/style-ems.css", newFileText);
